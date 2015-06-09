14c06db90a998fc78ed80f90ad428cfb7260327f
OperationXen <root@localhost.localdomain>
2015-06-09 08:37:47 -0400

    Added html and wiki style link title handling

--- frasmotic.py
+++ frasmotic.py
@@ -3,29 +3,41 @@ from sortedcontainers import SortedDict
 from datetime import datetime
 from string import whitespace as WHITESPACECHARS
 
-MaxLength = 20								#maximum word length allowed into dictionary
+MaxLength = 32								#maximum word length allowed into dictionary
 MinLength = 5								#minimum word length allowed into dictionary
-StripChars = ''
-SplitChars = '\' '							#possessive apostraphes
+StripChars = '[]'
+SplitChars = '\' '							#possessive apostrophes
 SplitChars+= '|\s'							#all whitespace	
 SplitChars+= '|\[|\]|\{|\}|\(|\)|\<|\>|\=|:|;|,|#|\"|\.|\||/|\\|\'|\x27|\`|\&|\x9C|\x9D|\x99|\xE2|\x98'		#all these characters
 AlphaChars = '[a-zA-Z]'
-QuotesString = '\".*?\"'
+QuotesString = '(\".+?\")'
+TitleString = '" ?>.+?</a'
+LinkString = '<a href=".*?".*?>.*?</'
+LinkTitleString = '">.*?</\Z'
+WikiLinkString = '\[\[.*?\]\]'
 
-ForceLowerCase = True
 ConcatonateChars = '-+_'
-GroupQuotations = True
-HTMLUnescape = False
-GroupTitles = True
+
+StripLine = False
+HTMLUnescape = True
+
+DoSingleWords = False
+ForceLowerCase = True
+GroupQuotations = False
+GroupTitles = False
+GroupLinkText = False
+GroupWikiLinks = True
+
 OmitNumericOnly = True
 IgnoredFileExtensions = [".inc", ".jpg", ".jpeg"]
+UseResumeFile = True
 
 # Text strings (usage etc)
 HEADER = "frasmotic creates wordlists from raw data, for example a site dump or folder of ebooks"
 USAGE = "Usage: python frasmotic.py <target file or folder>"
 INVALIDPATHERROR = "Target path does not exist."
 
-# Pre declare a bunch of lists, this approach allows for parallelism with inserts
+# Pre declare a bunch of lists, this approach allows for parallelism with inserts (which I gave up on)
 DictionaryA = SortedDict()
 DictionaryB = SortedDict()
 DictionaryC = SortedDict()
@@ -76,9 +88,14 @@ Dictionaries = dict([\
 ])
 
 FileList = { }
+#Precompiled regexes
 reSplitter = re.compile(SplitChars)
 reAlphaChars = re.compile(AlphaChars)
 reQuotes = re.compile(QuotesString)
+reTitles = re.compile(TitleString)
+reLinks = re.compile(LinkString)
+reLinkTitles= re.compile(LinkTitleString)
+reWikiLinks = re.compile(WikiLinkString)
 
 ##################################################################################
 #	Removes HTML encoding from a given line	(TODO: Optimise)		 #
@@ -86,8 +103,9 @@ reQuotes = re.compile(QuotesString)
 def UnescapeLine(Line):
 	Line = Line.replace("&lt;", "<")
 	Line = Line.replace("&gt;", ">")
-	Line = Line.replace("&quot;", "&")
+	Line = Line.replace("&quot;", "\"")
 	Line = Line.replace("&amp", "&")
+	Line = Line.replace("&#039;", "\'")
 	return(Line)
 
 ##################################################################################
@@ -104,13 +122,13 @@ def IdentifyDictionary(Word):
 ##################################################################################
 #       	Worker function to perform sorted insert in thread safe way	 #
 ##################################################################################
-def DoInsert(Word):
+def DoInsert(Word, Phrase):
 	try:
 		Length = len(Word)						#calculate length once
 		if((Length >= MaxLength) or (Length <= MinLength)):		#check word length is valid (never change at runtime)
 			return							#outside of defined limits, discard
 
-		if(ForceLowerCase):						#check if we have been asked to force to lower case
+		if(ForceLowerCase and not Phrase):				#check if we have been asked to force to lower case
 			Word = Word.lower()					#in which case we force it to lower
 
 		Dictionary = IdentifyDictionary(Word)				#identify the right dictionary for this word
@@ -122,30 +140,98 @@ def DoInsert(Word):
 		print(message)
 	return
 
+def CapitaliseString(String):
+	Output = ' '
+	try:
+		Words = String.split(' ')
+		CapitalizedWords = []
+		for Word in Words:
+			if(len(Word) > 2):	
+				TitleCaseWord = Word[0].upper() + Word[1:]
+			else:
+				TitleCaseWord = Word[0].upper()
+			CapitalizedWords.append(TitleCaseWord)
+		Output = ' '.join(CapitalizedWords)
+	except Exception as Message:
+		pass
+
+	return(Output)
+
 ##################################################################################
 #		Break a line of text down to words and sort them		 #
 ##################################################################################
 def CrunchLine(Line):
-	SplitLine = reSplitter.split(Line)				#precompiled regex split function
+	
+	if(HTMLUnescape):
+		Line = UnescapeLine(Line)
 
-	for Word in SplitLine:						#iterate through the elements now split out
-		if(Word == None):					#check that the word was recovered properly
-			continue					#discard this one and move to the next
+	if(DoSingleWords):
+		SplitLine = reSplitter.split(Line)				#precompiled regex split function
+		for Word in SplitLine:						#iterate through the elements now split out
+			if(Word == None):					#check that the word was recovered properly
+				continue					#discard this one and move to the next
 
-		if(OmitNumericOnly):					#check word for alpha characters before adding it
-			if(reAlphaChars.search(Word) == None):		#words without alpha characters get excluded
-				continue				#more efficient than using error handling approach...
-
-		DoInsert(Word)						#add word to dictionary
+			if(OmitNumericOnly):					#check word for alpha characters before adding it
+				if(reAlphaChars.search(Word) == None):		#words without alpha characters get excluded
+					continue				#more efficient than using error handling approach...
+	
+			DoInsert(Word, False)					#add word to dictionary
 
 
 	if(GroupQuotations):
 		Quotes = reQuotes.findall(Line)
 		for Quote in Quotes:
 			if(Quote != None):
-				Quote = re.sub('[\s\"]', '', Quote)	#remove all whitespace and quote marks from quotes
-				DoInsert(Quote)
-#				print(Quote)
+				Quote = re.sub('[\"\-\.,\xe2]', '', Quote)
+
+				Output = CapitaliseString(Quote)
+				UpperQuote = re.sub('[\s]', '', Output)	#remove all whitespace
+				DoInsert(UpperQuote, True)
+
+				Quote = re.sub('[\s]', '', Quote)	#remove all whitespace
+				DoInsert(Quote, False)
+
+	if(GroupTitles):
+		Titles = reTitles.findall(Line)
+		for Title in Titles:
+			if(Title == None):
+				break
+			
+			Title = re.sub('(\A" ?>)|(</a)', '', Title)
+			Title = re.sub('\(.*\)', '', Title)
+			Title = UnescapeLine(Title)
+			
+			print(Title)
+
+	if(GroupLinkText):
+		Links = reLinks.findall(Line)
+		if(Links != []):
+			print(Links)
+			for Link in Links:
+				LinkTitle = reLinkTitles.findall(Link)
+				for Title in LinkTitle:
+					Title = re.sub('(\A"?>)|(</\Z)', '', Title)	#remove the functional chars left at the start and end
+					Title = re.sub('\(.*?\)', '', Title)		#remove stuff in brackets
+					Title = UnescapeLine(Title)			#remove encoded html
+					Title = re.sub('\s', '', Title) 		#remove whitespace
+					Title = CapitaliseString(Title)
+					DoInsert(Title, False)
+					DoInsert(Title, True)
+					Title = re.sub('\W', '', Title)
+					DoInsert(Title, False)
+	
+	if(GroupWikiLinks):
+		Links = reWikiLinks.findall(Line)
+		if(Links != []):
+			for Link in Links:
+				Link = re.sub('(\A\[\[)|(\]\]\Z)', '', Link)
+				for Title in re.split('|', Link):
+					if(not ":" in Title):
+						Title = re.sub('\(.*?\)', '', Title)		#remove stuff in brackets
+						Title = CapitaliseString(Title)
+						Title = re.sub('[\s\W]', '', Title) 		#remove whitespace and punctuation
+						DoInsert(Title, True)
+						DoInsert(Title, False)
 
 ##################################################################################
 #			Process file, line by line				 #
@@ -156,8 +242,8 @@ def CreateWordList(Path, Position):
 			try:
 				if(HTMLUnescape):				#unescape any html encoding	
 					Line = UnescapeLine(Line)
-				if(len(StripChars)):				#if we have been given characters to strip
-					Line = Line.strip(StripChars)		#strip them before passing the line on to worker
+				if(StripLine):					#if we want to strip various chars
+					Line = re.sub(StripChars, '', Line)	#substitute them with nothing
 
 				CrunchLine(Line)				#data suitable for splitting and processing
 			except Exception as Message:
@@ -189,7 +275,7 @@ def UpdateDisplay():
 #		Create a resume file, or output the finished article		 #
 ##################################################################################
 def CreateOutputFile(Finished):
-	if not Finished:							#if the parameter is false, it means we're writing a resume file
+	if not Finished and UseResumeFile:					#if the parameter is false, it means we're writing a resume file
 		OutputFile = "words.resume"
 		with open(OutputFile, "w") as File:				#this syntax is recommended as "pythonic" apparently
 			for FileName, Position in FileList.iteritems():
@@ -237,6 +323,9 @@ def Init():
 	
 
 def ImportResumeFile():
+	if(not UseResumeFile):
+		return
+
 	try:
 		with open("words.resume", "r") as File:
 			print("Found resume file...")
